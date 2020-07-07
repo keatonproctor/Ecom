1c975900e19fc23d0ea7ae6e98f24c2e13901bb3
Keaton Proctor <keatonproctor@Keatons-MacBook-Pro.local>
2020-07-07 14:07:40 -0600

    Styled purchase detail

--- src/components/account/purchaseDetail.js
+++ src/components/account/purchaseDetail.js
@@ -17,7 +17,7 @@ class PurchaseDetail extends Component {
     render() {
         const { className, orderNumber, orderDate, user, total, creditCard } = this.props;
         const { name, shippingAddress } = user;
-        // const nameAddress = `${name}       ${shippingAddress}`
+        const nameAddress = `${name}       ${shippingAddress}`
         return (
             <div className={`${className} purchase-detail`}>
                 <PurchaseDetailLabel
@@ -31,7 +31,7 @@ class PurchaseDetail extends Component {
                 <PurchaseDetailLabel
                  className='purchase-detail__shipping-address'
                  title='Shipping Address'
-                 value={'${name}\n${shippingAddress}'}/>
+                 value={nameAddress}/>
                 <PurchaseDetailLabel
                  className='purchase-detail__total'
                  title='Total'
