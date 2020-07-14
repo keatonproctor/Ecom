dbbaec3235bf7877023e88908627b744d0f2c758
Keaton Proctor <keatonproctor@Keatons-MacBook-Pro.local>
2020-07-13 20:20:29 -0600

    Built shop cart component

--- src/components/shop/shop.js
+++ src/components/shop/shop.js
@@ -4,9 +4,18 @@ import { connect } from 'react-redux';
 import * as actions from '../../actions';
 import ShopSearchBar from './shopSearchBar';
 import ShopProduct from './shopProduct';
-// import ShopCart from './shopCart';
+import ShopCart from './shopCart';
+import CartButton from './cartButton';
 
 class Shop extends Component {
+
+    constructor() {
+        super()
+
+        this.state = {
+            showCart: true
+        }
+    }
 
     componentDidMount() {
         const headerLinks = [
@@ -18,6 +27,7 @@ class Shop extends Component {
         ]
         this.props.setHeaderLinks(headerLinks);
         this.props.fetchShopCategories();
+
         // filter products with links
         this.props.fetchShopProducts();
     }
@@ -27,14 +37,23 @@ class Shop extends Component {
             this.props.setNavbarLinks(nextProps.categories, (_id) => this.props.filterProductsWithCategoryId(_id));
         }
         return true
-
     }
 
     onSubmit = (fields) => {
         this.props.filterProductsWithQuery(fields)
     }
 
+    handleAddToCart = () => {
+        if(document.getElementById('shop-cart').classList.contains('cart-hidden')) {
+            document.getElementById('shop-cart').classList.remove('cart-hidden');
+        } else {
+            document.getElementById('shop-cart').classList.add('cart-hidden');
+        }
+    }
+
+
     render() {
+        // return <ShopCart className='shop__cart'/>
         return (
             <div className='shop'>
                 <ShopSearchBar onSubmit={this.onSubmit} className='shop__search-bar'/>
@@ -47,7 +66,11 @@ class Shop extends Component {
                         })
                     }
                 </div>
-                {/* shop cart button */}
+                {
+                    this.state.showCart ? <ShopCart className='shop__cart'/> : ''
+                }
+                
+                <CartButton onClick={this.handleAddToCart} className='shop__cart-button' icon='fas fa-cart-plus'/>
             </div>
         )
     }
